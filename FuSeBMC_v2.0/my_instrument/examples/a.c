46f41520d318cbd64d9e2e60965a1c28c47901d4
kaled Alshmrany <48326737+kaled-alshmrany@users.noreply.github.com>
2020-05-28 11:13:15 +0100

    Add files via upload

---
+++ FuSeBMC_v2.0/my_instrument/examples/a.c
@@ -0,0 +1,13 @@
+// not Compile.
+int main()
+{
+     volatile unsigned long long a = 3;
+      switch(8UL)
+      {
+         case 1UL:
+            //lbl:;
+            __asm__ ("movb %%gs:%1,%0": "=q" (a): "m" (a));
+            a++;
+      }
+    return 0;
+}
---
+++ FuSeBMC_v2.0/my_instrument/examples/c.cpp
@@ -0,0 +1,35 @@
+void f(){lbl:;}
+void f2(){}
+void f3(){}
+void f4(){}
+int main()
+{
+    /*int a=2,b=3,c=4,x=10,y=20,z=40;
+    if(x>1);else c++;
+    if(x>1);else;
+    if(x>10)y=300;
+    if(z>10){y=500;}        
+    if(a>1)x++;else c++;
+    if(a>1)x++;else {c++;};
+    if(a>1){x++;}else c++;
+    if(a>1){x++;}else {c++;}
+
+    if(x>10)y=300;
+    if(z>10){y=500;}        
+    if(a>1)x++;else ;
+    if(a>1)x++;else {;};
+    if(a>1){x++;}else ;
+    if(a>1){x++;}else {;}
+
+    if(x>10);
+    if(z>10){;}        
+    if(a>1);else c++;
+    if(a>1);else {c++;};
+    if(a>1){;}else c++;
+    if(a>1){;}else {c++;}
+    if(a>1){;}else {;}
+    if(a>100){a++;}
+    while(a>100)a++;*/
+    return 0;
+        
+}
