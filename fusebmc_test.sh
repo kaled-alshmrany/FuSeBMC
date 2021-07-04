1474397eabb3f22b19889f0de141881deabf5a2c
kaled Alshmrany <48326737+kaled-alshmrany@users.noreply.github.com>
2021-07-04 11:38:56 +0300

    Update fusebmc_test.sh

--- fusebmc_test.sh
+++ fusebmc_test.sh
@@ -5,5 +5,4 @@
 #sudo chmod o+wt /sys/fs/cgroup/cpuset
 #sudo chmod o+wt /sys/fs/cgroup/blkio/ /sys/fs/cgroup/cpu,cpuacct/ /sys/fs/cgroup/freezer/ /sys/fs/cgroup/memory/user.slice/user-1000.slice/session-1.scope
 #benchexec ./fusebmc.xml --timelimit 900s --tasks ReachSafety-Arrays --limitCores 1 --numOfThreads 1 --no-container --full-access-dir / --hidden-dir /home --hidden-dir  ./results-verified -d --no-compress-results
-#benchexec ./fusebmc.xml --timelimit 900s --tasks ReachSafety-Arrays --limitCores 1 --numOfThreads 1 --no-container --full-access-dir / --hidden-dir /home --hidden-dir  ./results-verified -d --no-compress-results
 benchexec ./fusebmc.xml --timelimit 900s --tasks ReachSafety-Floats --limitCores 1 --numOfThreads 1 --no-container --no-compress-results
