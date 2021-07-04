591e40d0c748c356c3ecbe65c9dead8fd86a89d6
kaled Alshmrany <48326737+kaled-alshmrany@users.noreply.github.com>
2021-07-04 11:40:43 +0300

    Update testcov_test.sh

--- testcov_test.sh
+++ testcov_test.sh
@@ -3,5 +3,4 @@ sudo chmod o+wt '/sys/fs/cgroup/cpu,cpuacct/'
 sudo chmod o+wt '/sys/fs/cgroup/freezer/'
 sudo chmod o+wt /sys/fs/cgroup/cpuset
 sudo chmod o+wt /sys/fs/cgroup/blkio/ /sys/fs/cgroup/cpu,cpuacct/ /sys/fs/cgroup/freezer/ /sys/fs/cgroup/memory/user.slice/user-1000.slice/session-1.scope
-#--tasks ReachSafety-Arrays 
 sudo benchexec ./testcov.xml --timelimit 900s --tasks SoftwareSystems-BusyBox-MemSafety --limitCores 1 --numOfThreads 1 --no-container --no-compress-results
